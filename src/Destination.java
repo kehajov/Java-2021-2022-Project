b87afc762ec1d2f33b8154abfa55d411adadf1aa
KrisK326 <kr.k.kostadinov@gmail.com>
2021-12-15 13:27:57 +0200

    Package foundation

---
+++ src/Destination.java
@@ -0,0 +1,4 @@
+public enum Destination {
+    OFFICE,
+    ADDRESS
+}
--- src/Package.java
+++ src/Package.java
@@ -1,3 +1,47 @@
+import java.util.Scanner;
+
 public class Package {
     //Крис
+
+    Scanner scanner = new Scanner(System.in);
+
+    private Client sender;
+    private Client receiver;
+    private final double weight;
+    private Destination destination;
+    private double price;
+    private String address;
+
+
+    public Package(Client sender, Client receiver, double weight, Destination destination) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.weight = weight;
+        this.destination = Destination.OFFICE;
+
+        if(destination == Destination.OFFICE) {
+            System.out.println("Please chose office which you'd like the package to be delivered to: ");
+
+            //Да се покаже лист от офиси
+
+            if(this.weight < 5){
+                this.price = 5.6;
+            } else {
+                this.price = 5 + ((weight - 5) * 0.6);
+            }
+
+            System.out.println("The price of delivery is: " + this.price);
+        } else {
+            System.out.println("Please enter address for delivery: ");
+            this.address = scanner.nextLine();
+                if(this.weight < 5) {
+                    this.price = 7;
+                } else {
+                    this.price = 7 + ((this.weight - 5) * 0.8);
+                }
+            System.out.println("The price of delivery is: " + this.price);
+        }
+    }
+
+
 }
