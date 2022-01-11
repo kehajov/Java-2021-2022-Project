c55ab59489b7747c060f7c20c578b7478cd26714
Dimanaa <95311278+Dimanaa@users.noreply.github.com>
2022-01-11 22:28:08 +0200

    Update Client.java

--- src/model/Client.java
+++ src/model/Client.java
@@ -5,10 +5,14 @@ public class Client {
 
     private String name;
     private String phone;
+    public List <Parcel> listOfSentParcels;
+    public List <Parcel> listOfReceivedParcels;
 
     public Client(String name, String phone) {
         this.name = name;
         this.phone = phone;
+        listOfReceivedParcels = new ArrayList();
+        listOfSentParcels = new ArrayList();
     }
 
     public String getName() {
@@ -25,5 +29,38 @@ public class Client {
 
     public void setPhone(String phone) {
         this.phone = phone;
+    }
+    
+    public List<Parcel> getListOfSentParcels() {
+        return listOfSentParcels;
+    }
+
+    public List<Parcel> getListOfReceivedParcels() {
+        return listOfReceivedParcels;
+    }
+    
+    public void addSentParcel (Parcel sentParcel){listOfSentParcels.add(sentParcel);}
+    public void addReceivedParcel (Parcel receivedParcel){listOfReceivedParcels.add(receivedParcel);}
+
+    public void displaySentParcels() {
+        if (listOfSentParcels.isEmpty()) {
+            System.out.println("There aren't any sent parcels.");
+        } else {
+            Iterator<Parcel> iterator = listOfSentParcels.iterator();
+            while (iterator.hasNext()) {
+                System.out.println(iterator.next() + " ");
+            }
+        }
+    }
+
+    public void displayReceivedParcels() {
+        if (listOfReceivedParcels.isEmpty()) {
+            System.out.println("There aren't any received parcels.");
+        } else {
+            Iterator<Parcel> iterator = llistOfReceivedParcels.iterator();
+            while (iterator.hasNext()) {
+                System.out.println(iterator.next() + " ");
+            }
+        }
     }
 }
