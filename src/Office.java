3b82e378064ceee20c1fdf8e5754cf93744339d0
Dimanaa <95311278+Dimanaa@users.noreply.github.com>
2022-01-11 22:41:54 +0200

    Update Office.java

--- src/Office.java
+++ src/Office.java
@@ -1,4 +1,17 @@
 public class Office {
    public int id;
    public String location;
+   
+   public Office(int id, String location) {
+        this.id = id;
+        this.location = location;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
 }
