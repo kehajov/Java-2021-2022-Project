5113401fdde5de05b9a28d8e814afe3d5422cb6b
veronika1035 <veronikadimitrova1035@gmail.com>
2022-01-18 21:40:09 +0200

    Veronika Employee

--- src/Employee.java
+++ src/Employee.java
@@ -2,6 +2,11 @@ public class Employee {
     private final TypeOfEmployee typeOfEmployee;
     private final String name;
 
+    public enum TypeOfEmployee {
+        COURIER,OFFICEWORKER
+    }
+
+
     public Employee(TypeOfEmployee typeOfEmployee, String name) {
         this.typeOfEmployee = typeOfEmployee;
         this.name = name;
--- src/TypeOfEmployee.java
+++
@@ -1,3 +0,0 @@
-public enum TypeOfEmployee {
-    COURIER,OFFICEWORKER
-}
